3de20babae004d0dd6dfba6d9d6e7ab164ad32ec
Brian Kauke <kaukeb@gmail.com>
2013-09-27 05:27:51 -0700

    Clean up moth problem, add comments

--- solutions/moth-eradication/cpp-brian/moth.cpp
+++ solutions/moth-eradication/cpp-brian/moth.cpp
@@ -60,21 +60,75 @@ bool compare_y(const point& a, const point& b)
     return a.y < b.y;
 }
 
+void reduce_to_convex_hull(point_vector& points)
+{
+    size_t n = points.size();
+    
+    // With 3 or fewer points, all must be on the hull, so nothing to do
+    if (n < 4)
+        return;
+    
+    // Allocate a mask vector to identify points on hull
+    std::vector<bool> on_hull(n, false);
+    
+    // Swap the lowest point into the first position
+    point_vector::iterator lowest_pos = std::min_element(points.begin(), points.end(), compare_y);
+    std::swap(*lowest_pos, points.front());
+    point lowest_point = points.front();
+    
+    // Sort points (except first) by angle from lowest point (in reverse order)
+    std::sort(points.begin() + 1, points.end(), clockwise_comparator(lowest_point));
+    
+    // The lowest point is always on the hull
+    on_hull[0] = true;
+    
+    // Perform scan to determine whether points are on the hull
+    for (point_vector::iterator it = points.begin() + 1; it < points.end() - 1; ++it)
+    {
+        // Size of vector is > 3, so this is safe
+        point a = *(it - 1);
+        point b = *it;
+        point c = *(it + 1);
+        
+        if (clockwise_or_collinear(a, b, c))
+            on_hull[it - points.begin()] = true;
+    }
+    
+    // Handle the last point separately, since it connects to the first
+    point_vector::iterator last = points.end() - 1;
+    if (clockwise_or_collinear(*(last - 1), *last, points.front()))
+        on_hull[last - points.begin()] = true;
+    
+    // Count the number of points on the hull
+    int n_hull = std::accumulate(on_hull.begin(), on_hull.end(), 0);
+    
+    // Compact the original point vector to contain only points on the hull
+    point_vector::iterator hp = points.begin();
+    for (int i = 0; i < points.size(); ++i)
+    {
+        if (on_hull[i])
+            *hp++ = points[i];
+    }
+    points.resize(n_hull);
+}
+
 int main(int argc, const char * argv[])
 {
     int n_vertices = 0;
     std::string line;
     while(!std::cin.eof())
     {
+        // Read the number of points in the region
         std::getline(std::cin, line);
         if (!(std::sscanf(line.c_str(), "%d", &n_vertices) == 1))
             exit(1);
         if (n_vertices == 0)
             break;
         
+        // Allocate storage for points
         point_vector points(n_vertices);
-        std::vector<bool> on_hull(n_vertices, false);
         
+        // Read points
         for (int i=0; i < n_vertices; ++i)
         {
             point p;
@@ -84,84 +138,25 @@ int main(int argc, const char * argv[])
             points[i] = p;
         }
         
-        //std::cout << "Before sort:";
-        //for (int i = 0; i < points.size(); ++i)
-        //{
-        //    std::cout << "(" << points[i].x << ", " << points[i].y << ") ";
-        //}
-        //std::cout << std::endl;
+        // Run convex hull algorithm
+        reduce_to_convex_hull(points);
         
-        // Swap the lowest point into the first position
-        point_vector::iterator lowest_pos = std::min_element(points.begin(), points.end(), compare_y);
-        std::swap(*lowest_pos, points.front());
-        point lowest_point = points.front();
-        
-        std::sort(points.begin() + 1, points.end(), clockwise_comparator(lowest_point));
-        
-        //std::cout << "After sort: ";
-        //for (int i = 0; i < points.size(); ++i)
-        //{
-        //    std::cout << "(" << points[i].x << ", " << points[i].y << ") ";
-        //}
-        //std::cout << std::endl;
-        
-        // The lowest point is always on the hull
-        on_hull[0] = true;
-        
-        // TODO: n=0, n=1
-        
-        // Exclude last point from the loop. Must handle degenerate n=0, 1 cases or this will crash!
-        for (point_vector::iterator it = points.begin() + 1; it < points.end() - 1; ++it)
-        {
-            point a = *(it - 1);
-            point b = *it;
-            point c = *(it + 1);
-            //std::cout << "clockwise test: "
-            //    << "(" << a.x << ", " << a.y << "), "
-            //    << "(" << b.x << ", " << b.y << "), "
-            //    << "(" << c.x << ", " << c.y << ")";
-            if (clockwise_or_collinear(a, b, c))
-            {
-                //std::cout << " TRUE";
-                on_hull[it - points.begin()] = true;
-            }
-            //std::cout << std::endl;
-        }
-        
-        // Last one
-        point_vector::iterator last = points.end() - 1;
-        if (clockwise_or_collinear(*(last - 1), *last, points.front()))
-        {
-            on_hull[last - points.begin()] = true;
-        }
-        
-        // Count the number of points on the hull
-        int n_hull = std::accumulate(on_hull.begin(), on_hull.end(), 0);
-        
-        // Compact the original point vector to contain only points on the hull
-        point_vector::iterator hp = points.begin();
-        for (int i = 0; i < points.size(); ++i)
-        {
-            if (on_hull[i])
-                *hp++ = points[i];
-        }
-        points.resize(n_hull);
-        
+        // Compute perimeter
         double perimeter = 0.0;
         for (point_vector::iterator it = points.begin(); it < points.end() - 1; ++it)
         {
             perimeter += distance(*it, *(it + 1));
         }
-        // Handle the last point
         perimeter += distance(points.back(), points.front());
         
+        // Print region perimeter
         for (point_vector::iterator it = points.begin(); it < points.end(); ++it)
         {
             std::cout << *it << "-";
         }
-        std::cout << points.front();
-        std::cout << std::endl;
+        std::cout << points.front() << std::endl;
         
+        // Print the perimeter length
         std::cout << std::setprecision(2) << "Perimeter length = " << perimeter << std::endl;
         
         std::cout << std::endl;
