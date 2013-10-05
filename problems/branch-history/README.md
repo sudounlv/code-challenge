ef2a4170ed72e04e0fdadce69dda006fd6d60491
Brian Kauke <kaukeb@gmail.com>
2013-10-04 17:18:07 -0700

    Branch history: Fix sample input, finish problem statement, add solution

--- problems/branch-history/README.md
+++ problems/branch-history/README.md
@@ -7,7 +7,14 @@ Input
 -----
 
 ### stdin
-The input file will provide a list of commits followed by a list of refs.
+The number of commits will be provided on a line, followed by all the commits,
+followed by the number of refs on its own line, followed by all the refs.
+
+#### Commits
+
+Commits consist of two lines of input followed by a blank line. The first
+line contains the indices of the parent commit(s). The second line contains
+the commit message.
 
 The commits form a DAG (directed acyclic graph). Each commit may have 0,
 1, or 2 parent commits, which are given as two integer indices. `-1` means
@@ -15,7 +22,10 @@ that the index is null and should be ignored.
 
 Commits are given in chronological order.
 
-Each ref will be specified by a name followed by a number which is the index
+#### Refs
+
+Each ref will be specified by one line of input. This will be a name
+(no spaces), followed by a space, followed by a number which is the index
 of the commit that it points to.
 
 ### args
@@ -40,8 +50,8 @@ be the name of the ref for which to print the log.
     3 -1
     4. Fixed 23 bugs in new generator
 
-    5 -1
-    5. Bug fix in bean counter
+    2 -1
+    5. Bug fix in load balancer
 
     4 5
     6. Merge bug fixes into wordgen branch
@@ -49,19 +59,19 @@ be the name of the ref for which to print the log.
     5 -1
     7. Optimize bean counter for performance
 
-    8 -1
+    7 -1
     8. Parallelize bean counter
 
-    5 9
+    5 8
     9. Merge parallel bean counter into master
 
-    10 -1
+    9 -1
     10. Update README
 
     3
-    master 11
-    wordgen 7
-    fastbeans 9
+    master 10
+    wordgen 6
+    fastbeans 8
 
 Note: Numbers at the start of commit messages are provided as a convenience for debugging.
 They do not need to be parsed; commits will always be provided in chronological order, starting
@@ -88,9 +98,8 @@ walk the DAG and print out the commit messages for each ancestor commit in
 Hints / Suggestions
 -------------------
 - Breadth-first traversal
-    - Priority queue sorted by index for open nodes
+    - Maintain a set sorted by index for open nodes
 - Depth-first traversal
-    - Less efficient
-    - Use std::set to de-duplicate commits
-    - Sort after traversal
+    - Use std::set to de-duplicate nodes
+    - Output after traversal
 
---
+++ solutions/branch-history/cpp-brian/branch_history.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <set>
+#include <functional>
+
+struct commit
+{
+    commit() {}
+    commit(int index, std::string& message, int p1, int p2)
+        : index(index), message(message)
+    {
+        parents[0] = p1;
+        parents[1] = p2;
+    }
+    int index;
+    int parents[2];
+    std::string message;
+};
+
+int main (int argc, char *argv[])
+{
+    if (argc != 2)
+        exit(1);
+    
+    std::string target_ref(argv[1]);
+    std::string line;
+    
+    int n_commits, n_refs;
+    
+    std::getline(std::cin, line);
+    std::istringstream(line) >> n_commits;
+    
+    std::vector<commit> commits(n_commits);
+    
+    for (int i=0; i < n_commits; i++)
+    {
+        int p1, p2;
+        std::getline(std::cin, line);
+        if (std::sscanf(line.c_str(), "%d %d", &p1, &p2) != 2)
+            exit(1);
+        std::getline(std::cin, line);
+        commits[i] = commit(i, line, p1, p2);
+        std::getline(std::cin, line);
+        if (line.length() != 0)
+            exit(1);
+    }
+    
+    std::getline(std::cin, line);
+    std::istringstream(line) >> n_refs;
+    
+    std::map<std::string, int> refs;
+    
+    for (int i=0; i < n_refs; i++)
+    {
+        int index;
+        char name_buf[line.length()];
+        std::getline(std::cin, line);
+        if (std::sscanf(line.c_str(), "%s %d", name_buf, &index) != 2)
+            exit(1);
+        refs[std::string(name_buf)] = index;
+    }
+    
+    int start_index = refs[target_ref];
+    
+    // Note: std::set is internally ordered and so can be used as a priority
+    // queue with unique elements. This is all that's needed for DAG traversal.
+    //
+    // However, here I'm using both a pqueue and a set to track all the discovered
+    // nodes. This "textbook" breadth-first traversal can handle general digraphs
+    // (with cycles), not only DAGs.
+    
+    std::priority_queue<int> open_nodes;
+    std::set<int> discovered_nodes;
+    
+    open_nodes.push(start_index);
+    
+    while (!open_nodes.empty())
+    {
+        commit &c = commits[open_nodes.top()];
+        open_nodes.pop();
+        std::cout << c.message << std::endl;
+        for (int i=0; i < 2; i++)
+        {
+            int p = c.parents[i];
+            if (p != -1 && discovered_nodes.find(p) == discovered_nodes.end())
+            {
+                open_nodes.push(p);
+                discovered_nodes.insert(p);
+            }
+        }
+    }
+    
+    return 0;
+}
